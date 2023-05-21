332380e40b65a6df0b252aafb420edccbd79ec49
mindulle <mindullestudio@gmail.com>
2023-05-21 10:27:29 +0000

    test: test that is possbile to publish to github page with this files

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,21 @@
 import * as THREE from 'three'
 import ReactDOM from 'react-dom/client'
-import React, { useRef, useState } from 'react'
+import { useRef, useState } from 'react'
 import { Canvas, useFrame, ThreeElements } from '@react-three/fiber'
 
 function Box(props: ThreeElements['mesh']) {
   const ref = useRef<THREE.Mesh>(null!)
   const [hovered, hover] = useState(false)
   const [clicked, click] = useState(false)
-  useFrame((state, delta) => (ref.current.rotation.x += delta))
+  useFrame((_, delta) => (ref.current.rotation.x += delta))
   return (
     <mesh
       {...props}
       ref={ref}
       scale={clicked ? 1.5 : 1}
-      onClick={(event) => click(!clicked)}
-      onPointerOver={(event) => hover(true)}
-      onPointerOut={(event) => hover(false)}>
+      onClick={() => click(!clicked)}
+      onPointerOver={() => hover(true)}
+      onPointerOut={() => hover(false)}>
       <boxGeometry args={[1, 1, 1]} />
       <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
     </mesh>
